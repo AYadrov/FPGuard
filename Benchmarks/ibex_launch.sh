fd80852e3293f694836d838d4bc00f896efc688d
AYadrov <kowalski228@mail.ru>
2023-06-08 17:06:54 -0600

    Updated Benchmarks repository

--- Benchmarks/ibex_launch.sh
+++ Benchmarks/ibex_launch.sh
@@ -27,127 +27,127 @@ export SAT_ROOT=${SAT_ROOT_}
 
 cd ../src
 echo ${testname1}
-python3 satire+.py  \
+python3 fpguard.py  \
     --std \
 		--file ${testname1} \
 		--enable-constr
 
 echo ${testname2}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname2} \
 		--enable-constr
 
 echo ${testname3}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname3} \
 		--enable-constr
 
 echo ${testname4}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname4} \
 		--enable-constr
 
 echo ${testname5}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname5} \
 		--enable-constr
 
 echo ${testname6}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname6} \
 		--enable-constr
 
 echo ${testname7}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname7} \
 		--enable-constr
 
 echo ${testname8}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname8} \
 		--enable-constr
 
 echo ${testname9}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname9} \
 		--enable-constr
 
 echo ${testname10}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname10} \
 		--enable-constr
 
 echo ${testname11}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname11} \
 		--enable-constr
 
 echo ${testname12}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname12} \
 		--enable-constr
 
 echo ${testname13}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname13} \
 		--enable-constr
 
 echo ${testname14}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname14} \
 		--enable-constr
 
 echo ${testname15}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname15} \
 		--enable-constr
 
 echo ${testname16}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname16} \
 		--enable-constr
 
 echo ${testname17}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname17} \
 		--enable-constr
 
 echo ${testname18}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname18} \
 		--enable-constr
 
 echo ${testname19}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname19} \
 		--enable-constr
 
 echo ${testname20}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 		--file ${testname20} \
 		--enable-constr
 
 echo ${testname21}
-python3 satire+.py  \
+python3 fpguard.py  \
         --std \
 	    --file ${testname21} \
 	    --enable-constr
